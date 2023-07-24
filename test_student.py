52d8bdd3cf382e68229d95703af84199df503281
Pur3-MAlice <aliceford98@live.co.uk>
2023-07-24 17:35:33 +0000

    creates basic funtion to pass email test

--- test_student.py
+++ test_student.py
@@ -5,8 +5,12 @@ class TestStudent(unittest.TestCase):
     
     def test_full_name(self):
         student = Student('John', 'Doe')
-
         self.assertEqual(student.full_name, 'John Doe')
+
+
+    def test_email(self):
+        student = Student('John', 'Doe')
+        self.assertEqual(student.email, 'john.doe@email.com')    
 
 
     def test_alert_santa(self):
